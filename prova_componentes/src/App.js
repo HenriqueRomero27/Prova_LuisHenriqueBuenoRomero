274fb7fe3e827464f370c8bc221192df52eb9049
Henrique Romero <luishenriquebuenoromero@gmail.com>
2023-09-19 16:50:12 +0000

    create first step of the test

---
+++ prova_componentes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Noticia from './componentes/Noticia';
+
+function App() {
+  return (
+    <div className="Container">
+      <Noticia />
+    </div>
+  );
+}
+
+export default App;
---
+++ prova_componentes/src/componentes/Noticia.js
@@ -0,0 +1,11 @@
+function Noticia() {
+    return (
+        <div className="noticiaContent">
+            <h1>Mano Menezes tem negociação para ser técnico da seleção do Paraguai</h1>
+            <p>O técnico Mano Menezes tem uma negociação em curso para assumir a seleção paraguaia.</p>
+            <p>O nome do treinador brasileiro foi levado à entidade paraguaia por um empresário local, Santiago Mereles. Depois, com o caminho aberto, o contato com o staff de Mano foi feito.</p>
+        </div>
+    )
+}
+
+export default Noticia
